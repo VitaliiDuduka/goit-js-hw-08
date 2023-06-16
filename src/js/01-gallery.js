7f6e9879dba1cbc4d3b28b8f6b8a7360ee599d95
Vitalii Dududka <vitalyd_2001@yahoo.com>
2023-06-16 08:02:24 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,38 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
 
-console.log('Hello!');
+const imagesGallery = document.querySelector(".gallery");
+
+const markupImages = galleryItems
+  .map((image) => {
+    return (
+      '<li class="gallery__item"><a class="gallery__link" href="' +
+      image.original +
+      '"><img class="gallery__image" src="' +
+      image.preview +
+      '" alt="' +
+      image.description +
+      '"/></a></li>'
+    );
+  })
+  .join("");
+
+imagesGallery.insertAdjacentHTML("beforeend", markupImages);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionsDelay: 250,
+    disableScroll: false,
+    overlayOpacity: 0.9,
+    captionPosition: 'top',
+    navText: ['<<','>>'],
+});
+lightbox.on("show.lightbox", function (event) {
+  event.preventDefault();
+});
