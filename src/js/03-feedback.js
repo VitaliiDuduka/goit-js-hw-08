8b9e022ff9b6c23cf516020b95e0608fc7204492
Vitalii Dududka <vitalyd_2001@yahoo.com>
2023-06-19 09:39:30 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,40 +1,38 @@
-// import _throttle from 'lodash.throttle';
+import _throttle from 'lodash.throttle';
 
-// const formEl = document.querySelector('form');
-// const emailInpt = document.querySelector('input');
-// const messageInpt = document.querySelector('textarea');
+const feedbackForm = document.querySelector('.feedback-form');
+const emailInput = document.querySelector("[name='email']");
+const messageInput = document.querySelector("[name='message']");
 
-// formEl.addEventListener('input', _throttle(onFormInput, 500));
+feedbackForm.addEventListener('input', _throttle(onFormInput, 500));
 
-// function onFormInput() {
-//   const savedFormValues = {
-//     email: emailInpt.value,
-//     message: messageInpt.value,
-//   };
+function onFormInput() {
+  const currentFormValues = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
 
-//   localStorage.setItem('feedback-form-state', JSON.stringify(savedFormValues));
-// }
+  localStorage.setItem('feedback-form-state', JSON.stringify(currentFormValues));
+}
 
-// window.onload = () => {
-//   if (!localStorage.getItem('feedback-form-state')) {
-//     return;
-//   }
-//   const savedFormObj = JSON.parse(localStorage.getItem('feedback-form-state'));
+window.onload = () => {
+  if (!localStorage.getItem('feedback-form-state')) {
+    return;
+  }
+  emailInput.value = JSON.parse(localStorage.getItem('feedback-form-state')).email;
+  messageInput.value = JSON.parse(localStorage.getItem('feedback-form-state')).message;
+};
 
-//   emailInpt.value = savedFormObj.email;
-//   messageInpt.value = savedFormObj.message;
-// };
+feedbackForm.addEventListener('submit', onFormSubmit);
 
-// formEl.addEventListener('submit', onFormSubmit);
-
-// function onFormSubmit(event) {
-//   event.preventDefault();
-//   if (emailInpt.value === '' || messageInpt.value === '') {
-//     window.alert('Please fullfill both fields');
-//     return;
-//   }
-//   console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
-//   localStorage.removeItem('feedback-form-state');
-//   emailInpt.value = '';
-//   messageInpt.value = '';
-// }
+function onFormSubmit(event) {
+  event.preventDefault();
+  if (emailInput.value === '' || messageInput.value === '') {
+    window.alert('Please input both email and message');
+    return;
+  }
+    console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+    emailInput.value = '';
+    messageInput.value = '';
+    localStorage.removeItem('feedback-form-state');
+}
